6cc47bec2380200b6695b50dbe07460af7691d94
tibbyx <timohedtke@gmx.de>
2024-06-06 19:28:12 +0200

    added .env for api-key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
+
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: 'dc74db818f9d40dcb41db3765739128b'
+        key: import.meta.env.VITE_RAWG_API_KEY
     }
 })
