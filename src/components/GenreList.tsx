d5565d2635ce6611926d6e8838861be9a313ab68
tibbyx <timohedtke@gmx.de>
2024-06-08 18:56:02 +0200

    display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { HStack, List, ListItem, Image, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import { Genre } from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/imgage-url";
 
 function GenreList() {
   const { data, error, isLoading } = useData<Genre>("/genres");
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize='32px'
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
 
