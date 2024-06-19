4f39162a40c8c85ddfc5b76d8f0f8e6c8cdcebf7
tibbyx <timohedtke@gmx.de>
2024-06-19 17:38:50 +0200

    refactoring: fetch data with react query

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
@@ -13,7 +13,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error) return <Text>{error.message}</Text>;
 
   return (
     <SimpleGrid
@@ -27,7 +27,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <GameCardContainer key={game.id}>
           <GameCard game={game} />
         </GameCardContainer>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-export default axios.create({
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: import.meta.env.VITE_RAWG_API_KEY
-    }
-})
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY,
+  },
+});
