f653b1720fbd7ecb6c9d3540042f0ced696f297d
tibbyx <timohedtke@gmx.de>
2024-07-10 14:41:38 +0200

    build expandable text

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 import { Spinner } from "@chakra-ui/spinner";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,7 +15,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game?.name}</Heading>
-      <Text>{game?.description_raw}</Text>
+      <ExpandableText>{game!.description_raw}</ExpandableText>
     </>
   );
 };
